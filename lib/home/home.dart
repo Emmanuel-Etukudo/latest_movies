ce1d8a56f41815787771290a676626c601b0fe18
Etukudo Emmanuel <etukzbong@gmail.com>
2022-03-19 15:10:54 +0100

    implemented MyHomePage class used a FutureProvider to manage state

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,84 +1,123 @@
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+import 'dart:ui';
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latest_movies/home/movie.dart';
+import 'package:latest_movies/home/movie_service.dart';
+import 'package:latest_movies/home/movies_exception.dart';
 
-  final String title;
+final moviesFutureProvider =
+    FutureProvider.autoDispose<List<Movie>>((ref) async {
+  ref.maintainState = true;
+  final movieService = ref.read(movieServiceProvider);
+  final movies = await movieService.getMovies();
+  return movies;
+});
 
+class MyHomePage extends ConsumerWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<Movie>> movies = ref.watch(moviesFutureProvider);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Movisio"),
+        ),
+        body: movies.when(
+            data: (moviesData) {
+              return RefreshIndicator(
+                onRefresh: () { return ref.refresh(moviesFutureProvider.future); },
+                child: GridView.extent(
+                    maxCrossAxisExtent: 200,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                  childAspectRatio: 0.7,
+                  children: moviesData.map((movie) => _MovieBox(movie: movie)).toList(),
+                ),
+              );
+            },
+            error: (e, s) {
+              if(e is MoviesException){
+                return _ErrorBody(message: e.message!);
+              }
+              return _ErrorBody(message: "Oops something unexpected happened");
+            },
+            loading: () => const Center(
+                  child: CircularProgressIndicator(),
+                )));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ErrorBody extends ConsumerWidget {
+  const _ErrorBody({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String message;
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<Movie>> movies = ref.watch(moviesFutureProvider);
+    return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(message),
+        ElevatedButton(onPressed: (){return ref.refresh(moviesFutureProvider.future);}, child: Text("Try Again"))
+      ],
+    ),
     );
   }
 }
+
+class _MovieBox extends StatelessWidget {
+  final Movie movie;
+  const _MovieBox({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          movie.fullImageUrl,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: _FrontBanner(text: movie.originalTitle,))
+      ],
+    );
+  }
+}
+
+class _FrontBanner extends StatelessWidget {
+  final String text;
+  const _FrontBanner({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          color: Colors.grey.shade200.withOpacity(0.5),
+          height: 60,
+          child: Center(
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
